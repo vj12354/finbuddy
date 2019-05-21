08e87652b7c514b3a1b591c786e26b2bf8ea480f
Vijay <vijay.gangoni@lloydsbanking.com>
2019-05-21 11:03:11 +0100

    0.1: basic JPA entity.

---
+++ webapp/application/src/main/java/com/lbg/finbuddy/FinbuddyApplication.java
@@ -0,0 +1,23 @@
+package com.lbg.finbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = FinbuddyApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class FinbuddyApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(FinbuddyApplication.class, args);
+    }
+}
